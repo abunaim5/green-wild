b9a8ee4ea957506e686a348cb2cbc6bce7a1fe63
Abu Naim <abunaim140@gmail.com>
2024-11-29 23:38:35 +0600

    added dialog modal for add animal and add category

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,20 +5,28 @@ import Button from "@/components/Button";
 import useAnimals from "@/hooks/useAnimals";
 import useCategories from "@/hooks/useCategories";
 import { useState } from "react";
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog"
 
 export default function Home() {
   // const category = ['Land Animal', 'Bird', 'Fish', 'Insect']
   const [category, setCategory] = useState<string>('all')
   const { categories, categoryLoading } = useCategories();
-  const { animals, animalLoading } = useAnimals({category});
+  const { animals, animalLoading } = useAnimals({ category });
 
   const handleCategory = (category: string) => {
     console.log(category);
     setCategory(category)
-  }
-  console.log(category);
+  };
 
-  console.log(animals);
+  const handleAddCategory = e => {
+    e.preventDefault();
+  }
 
   return (
     <div className="h-[100vh]">
@@ -29,8 +37,30 @@ export default function Home() {
           }
         </div>
         <div className="flex gap-6 text-white">
-          <button className="border-[1px] rounded-full px-5 py-4 min-w-[100px] text-white">Add Animal</button>
-          <button className="border-[1px] rounded-full px-5 py-4 min-w-[100px] text-white">Add Category</button>
+          <Dialog>
+            <DialogTrigger className="border-[1px] rounded-full px-5 py-4 min-w-[100px] text-white">Add Animal</DialogTrigger>
+            <DialogContent>
+              <DialogHeader>
+                <DialogTitle>Add Animal</DialogTitle>
+              </DialogHeader>
+              <form onSubmit={handleAddCategory}>
+                <input type="text" />
+                <button type="submit">Save</button>
+              </form>
+            </DialogContent>
+          </Dialog>
+          <Dialog>
+            <DialogTrigger className="border-[1px] rounded-full px-5 py-4 min-w-[100px] text-white">Add Category</DialogTrigger>
+            <DialogContent>
+              <DialogHeader>
+                <DialogTitle>Add Category</DialogTitle>
+              </DialogHeader>
+              <form className="flex flex-col">
+                <input type="text" placeholder='Name' className="bg-gray-100 px-4 py-2 rounded-md" />
+                <button type="submit" className="bg-black text-white rounded-md mt-4 py-2 cursor-pointer">Save</button>
+              </form>
+            </DialogContent>
+          </Dialog>
         </div>
       </div>
       <div className="grid grid-cols-6 gap-8 mt-20">
