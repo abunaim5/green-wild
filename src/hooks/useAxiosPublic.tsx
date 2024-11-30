57698ee53d6fea6ed5a51fa7f3ceb5a77ada596d
Abu Naim <abunaim140@gmail.com>
2024-11-30 22:12:26 +0600

    added hosting server side url

--- src/hooks/useAxiosPublic.tsx
+++ src/hooks/useAxiosPublic.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosPublic = axios.create({
-    baseURL: 'http://localhost:5000'
+    baseURL: 'https://green-wild-server.vercel.app'
 });
 
 const useAxiosPublic = () => {
